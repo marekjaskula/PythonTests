4dd9d7c1a7f4ad516ba750d62d6fd5ccae76cd6a
Marek Jaskuła <Marek.Jaskula@zut.edu.pl>
2018-04-26 09:00:15 +0200

    0.02 list, funkctions, optional arguments, *arg, **karg 0.01 Test Git integration with PyCharm

--- Ex01.py
+++ Ex01.py
@@ -2,5 +2,24 @@ def  ShowVariable(vari):
     print(vari, type(vari))
 lista = [100, 20, 5]
 
-ShowVariable(lista)
 
+slownik = {
+    "Samochód" : "Citroen",
+    "Typ" : "C5"
+}
+
+ShowVariable(lista)
+ShowVariable(slownik)
+
+def Wyswietl(vari, lista_var= None, *args, **kargs):
+    print("START -> ")
+    ShowVariable(vari)
+    if lista_var:
+        for element in lista_var:
+            ShowVariable(element)
+    print("Args =", args)
+    print("KArgs = ", kargs)
+    print("STOP")
+
+
+Wyswietl(1, [1,2,3], 'arg3', 'arg4', arg5=12, arg6={"stos": "stosik"})
